51206c157eea54cd1b6daf5c6e10325cbc2aad8b
YUNH7 <yunny126@gmail.com>
2023-08-29 11:38:14 +0900

    refactor: assets index 파일 생성

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+export { default as blankbucket } from './blankbucket.png';
+export { default as loading } from './loading.gif';
+export { default as logo_black } from './logo_black.png';
+export { default as logo_white } from './logo_white.png';
+export { default as profile } from './profile.png';
