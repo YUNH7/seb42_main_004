9b5ddd28c7f3cec97d7026b6778afa1963c730fa
YUNH7 <yunny126@gmail.com>
2023-08-29 17:51:19 +0900

    refactor: 고정 데이터 컴포넌트 밖으로 이동

--- src/components/survey/SurveyPage3.js
+++ src/components/survey/SurveyPage3.js
@@ -9,6 +9,18 @@ import SurveyBox from './SurveyBox';
 import DietInfo from './DietInfo';
 import { setSurveyRcmd } from '../../reducers/surveyRcmdReducer';
 
+let explanation = {
+  easy: [
+    '다이어트는 너무 길어지면 힘들지만 무작정 빨리갈 수는 없어요.',
+    '천천히 목표를 향해 나아가보아요.',
+  ],
+  normal: ['이왕 다이어트를 하는거라면 조금은 도전적인 선택도 좋을거예요!'],
+  hard: [
+    '다이어트에 대한 강한 의지나 다가오는 중요한 일정이 있으신가요?',
+    '강도가 높아 지키기 힘들 수도 있어요.',
+  ],
+};
+
 function SurveyPage3() {
   let dispatch = useDispatch();
   let navigate = useNavigate();
@@ -30,18 +42,6 @@ function SurveyPage3() {
         dispatch(setReset());
         navigate(`/survey/result`);
       });
-  };
-
-  let explanation = {
-    easy: [
-      '다이어트는 너무 길어지면 힘들지만 무작정 빨리갈 수는 없어요.',
-      '천천히 목표를 향해 나아가보아요.',
-    ],
-    normal: ['이왕 다이어트를 하는거라면 조금은 도전적인 선택도 좋을거예요!'],
-    hard: [
-      '다이어트에 대한 강한 의지나 다가오는 중요한 일정이 있으신가요?',
-      '강도가 높아 지키기 힘들 수도 있어요.',
-    ],
   };
 
   let optionItem = Object.keys(explanation).map((p) => {
